be8c0c53c47783a21c35ffeba6157a4f4728511f
EloxFire <enzo.avagliano@ynov.com>
2021-11-19 13:58:23 +0100

    Renamed all tabs to fit application idea

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Home from './pages/Home/Home';
+import Profil from './pages/Profil/Profil';
+import Stats from './pages/Stats/Stats';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -38,31 +38,32 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/profil">
+            <Profil />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/overview">
+            <Home />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route path="/stats">
+            <Stats />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/overview" />
           </Route>
         </IonRouterOutlet>
+
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+          <IonTabButton tab="Profil" href="/profil">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Profil</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab="Overview" href="/overview">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Overview</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
+          <IonTabButton tab="Stats" href="/stats">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Statistiques</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
